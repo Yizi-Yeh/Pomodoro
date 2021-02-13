c6569409ce686223c3517a3d9e42ad382fe32f91
Yizi-Yeh <g384357@gmail.com>
2021-02-13 21:25:03 +0800

    pomodoro

---
+++ dist/precache-manifest.1d5bd3a822fb76373c015066ed5e6fc0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f617fa46c2f737bc30",
+    "url": "css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "3782ced0dfd3a7d714402fcfe141ae6e",
+    "url": "index.html"
+  },
+  {
+    "revision": "f0db505dc6a7e88813f1",
+    "url": "js/app.07a94606.js"
+  },
+  {
+    "revision": "09f617fa46c2f737bc30",
+    "url": "js/chunk-vendors.c585246f.js"
+  },
+  {
+    "revision": "4778b4e44dd40bbfa21d",
+    "url": "js/list.d73268b3.js"
+  },
+  {
+    "revision": "ace704668b0def3ed4ba",
+    "url": "js/settings.4c2b3c8e.js"
+  },
+  {
+    "revision": "982672f70a8ae0b8927c7e9ed7d470cb",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "sounds/alarm1.mp3"
+  },
+  {
+    "revision": "46c744f5e53a2259c87f4b029cb43e03",
+    "url": "sounds/yay.mp3"
+  }
+]);
